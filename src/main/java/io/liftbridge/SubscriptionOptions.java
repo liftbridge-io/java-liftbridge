c1cf9b815ef8c8470d9294299a95cacf602f5ea4
Caio Oliveira <caioaao@gmail.com>
2019-12-22 22:47:16 -0500

    subscription options

--- src/main/java/io/liftbridge/SubscriptionOptions.java
+++ src/main/java/io/liftbridge/SubscriptionOptions.java
@@ -1,12 +1,16 @@
 package io.liftbridge;
 
 import io.liftbridge.proto.Api.SubscribeRequest;
+import io.liftbridge.proto.Api;
 
 public class SubscriptionOptions {
     private SubscribeRequest.Builder requestBuilder;
+    private StartPosition startPosition;
     private MessageHandler messageHandler;
+
     public SubscriptionOptions() {
         requestBuilder = SubscribeRequest.newBuilder();
+        this.setStartPosition(new StartAtNewOnly());
     }
 
     /**
@@ -17,15 +21,92 @@ public class SubscriptionOptions {
         return this;
     }
 
-    public int getPartition() {
+    public int getPartitions() {
         return requestBuilder.getPartition();
-    }
-
-    public SubscriptionOptions setStartPosition() {
-        return this;
     }
 
     public MessageHandler getMessageHandler() {
         return messageHandler;
+    }
+
+    public String getStreamName() {
+        return requestBuilder.getStream();
+    }
+
+    public StartPosition getStartPosition() {
+        return this.startPosition;
+    }
+
+    public SubscriptionOptions setPartition(Integer partition) {
+        requestBuilder.setPartition(partition);
+        return this;
+    }
+
+    public SubscriptionOptions setMessageHandler(MessageHandler handler) {
+        this.messageHandler = handler;
+        return this;
+    }
+
+    public SubscriptionOptions setStreamName(String streamName) {
+        requestBuilder.setStream(streamName);
+        return this;
+    }
+
+    public SubscriptionOptions setStartPosition(StartPosition position) {
+        requestBuilder = position.setRequestBuilderParameters(requestBuilder);
+        this.startPosition = position;
+        return this;
+    }
+
+    SubscribeRequest asRequest() {
+        return requestBuilder.build();
+    }
+
+    public abstract class StartPosition {
+        protected abstract SubscribeRequest.Builder setRequestBuilderParameters(SubscribeRequest.Builder builder);
+    }
+
+    public class StartAtNewOnly extends StartPosition {
+        StartAtNewOnly() {}
+        protected SubscribeRequest.Builder setRequestBuilderParameters(SubscribeRequest.Builder builder) {
+            return builder.setStartPosition(Api.StartPosition.NEW_ONLY);
+        }
+    }
+
+    public class StartAtEarliestReceived extends StartPosition {
+        protected SubscribeRequest.Builder setRequestBuilderParameters(SubscribeRequest.Builder builder) {
+            return builder.setStartPosition(Api.StartPosition.EARLIEST);
+        }
+    }
+
+    public class StartAtLatestReceived extends StartPosition {
+        protected SubscribeRequest.Builder setRequestBuilderParameters(SubscribeRequest.Builder builder) {
+            return builder.setStartPosition(Api.StartPosition.LATEST);
+        }
+    }
+
+    public class StartAtOffset extends StartPosition {
+        private Long offset;
+
+        StartAtOffset(Long offset) {
+            this.offset = offset;
+        }
+
+        protected SubscribeRequest.Builder setRequestBuilderParameters(SubscribeRequest.Builder builder) {
+            return builder.setStartPosition(Api.StartPosition.OFFSET).setStartOffset(offset);
+        }
+    }
+
+    public class StartAtTimestamp extends StartPosition {
+        private Long timestampNanoSeconds;
+
+        StartAtTimestamp(Long timestampNanoSeconds) {
+            this.timestampNanoSeconds = timestampNanoSeconds;
+        }
+
+        protected SubscribeRequest.Builder setRequestBuilderParameters(SubscribeRequest.Builder builder) {
+            return builder.setStartPosition(Api.StartPosition.TIMESTAMP)
+                .setStartTimestamp(this.timestampNanoSeconds);
+        }
     }
 }
