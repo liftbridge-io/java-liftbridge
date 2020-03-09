8d8e17a06020707c5b461e8b48de3cf093d4d418
Caio Oliveira <caioaao@gmail.com>
2020-03-09 10:43:31 -0400

    uncomment and fix other subscribe tests

--- src/test/java/io/liftbridge/ClientSubscribeTest.java
+++ src/test/java/io/liftbridge/ClientSubscribeTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.awaitility.Awaitility.await;
 import static java.util.concurrent.TimeUnit.*;
-
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 import java.nio.ByteBuffer;
 import java.util.List;
@@ -88,73 +88,75 @@ public class ClientSubscribeTest extends BaseClientTest {
                           streamValues.toArray(vals));
     }
 
-    // @Test
-    // public void testSubscribeFromLatestReceived() {
-    //     SubscriptionOptions opts = new SubscriptionOptions()
-    //         .setStartPosition(new SubscriptionOptions.StartAtLatestReceived());
-    //     final AtomicLong lastOffset = new AtomicLong(-1);
+    @Test
+    public void testSubscribeFromLatestReceived() {
+        SubscriptionOptions opts = new SubscriptionOptions()
+            .setStartPosition(new SubscriptionOptions.StartAtLatestReceived());
+        final AtomicLong lastOffset = new AtomicLong(-1);
 
-    //     client.subscribe(populatedStreamName, new MessageHandler(){
-    //             public void onMessage(io.liftbridge.Message msg){
-    //                 if(msg.getValue().length > 0) {
-    //                     lastOffset.set(msg.getOffset());
-    //                 }
-    //             }
-    //             public void onError(Throwable t){}
-    //         }, opts);
+        client.subscribe(populatedStreamName, new MessageHandler(){
+                public void onMessage(io.liftbridge.Message msg){
+                    if(msg.getValue().length > 0) {
+                        lastOffset.set(msg.getOffset());
+                    }
+                }
+                public void onError(Throwable t){}
+            }, opts);
 
 
-    //     await().atMost(5, SECONDS).until(() -> lastOffset.get() >= 0);
-    //     assertEquals("Last offset message was received", 9, lastOffset.get());
-    // }
+        await().atMost(5, SECONDS).until(() -> lastOffset.get() >= 0);
+        assertEquals("Last offset message was received", 9, lastOffset.get());
+    }
 
-    // @Test
-    // public void testSubscribeFromNewOnly() {
-    //     SubscriptionOptions opts = new SubscriptionOptions();
-    //     final List<Integer> streamValues = new ArrayList<Integer>();
+    @Test
+    public void testSubscribeFromNewOnly() {
+        SubscriptionOptions opts = new SubscriptionOptions();
+        final List<Integer> streamValues = new ArrayList<Integer>();
 
-    //     client.subscribe(populatedStreamName, new MessageHandler(){
-    //             public void onMessage(io.liftbridge.Message msg){
-    //                 if(msg.getValue().length > 0) {
-    //                     streamValues.add(ByteBuffer.wrap(msg.getValue()).getInt());
-    //                 }
-    //             }
-    //             public void onError(Throwable t){}
-    //         }, opts);
+        client.subscribe(populatedStreamName, new MessageHandler(){
+                public void onMessage(io.liftbridge.Message msg){
+                    if(msg.getValue().length > 0) {
+                        streamValues.add(ByteBuffer.wrap(msg.getValue()).getInt());
+                    }
+                }
+                public void onError(Throwable t){}
+            }, opts);
 
-    //     for(Integer i = 0; i < 10; ++i) {
-    //         byte[] payload = ByteBuffer.allocate(20).putInt(i + 10).array();
-    //         client.publish(this.populatedStreamName,
-    //                        payload, 10, MILLISECONDS);
-    //     }
-    //     await().atMost(5, SECONDS).until(() -> streamValues.size() >= 10);
-    //     Collections.sort(streamValues);
-    //     Integer[] vals = new Integer[10];
-    //     assertArrayEquals("All messages were received",
-    //                       new Integer[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
-    //                       streamValues.toArray(vals));
-    // }
+        MessageOptions msgOpts = new MessageOptions()
+            .setAckDeadlineDuration(10)
+            .setAckDeadlineTimeUnit(MILLISECONDS);
+        for(Integer i = 0; i < 10; ++i) {
+            byte[] payload = ByteBuffer.allocate(20).putInt(i + 10).array();
+            client.publish(this.populatedStreamName, payload, msgOpts);
+        }
+        await().atMost(5, SECONDS).until(() -> streamValues.size() >= 10);
+        Collections.sort(streamValues);
+        Integer[] vals = new Integer[10];
+        assertArrayEquals("All messages were received",
+                          new Integer[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
+                          streamValues.toArray(vals));
+    }
 
-    // @Test
-    // public void testSubscribeFromOffset() {
-    //     SubscriptionOptions opts = new SubscriptionOptions()
-    //         .setStartPosition(new SubscriptionOptions.StartAtOffset(5L));
-    //     final List<Long> offsets = new ArrayList<Long>();
+    @Test
+    public void testSubscribeFromOffset() {
+        SubscriptionOptions opts = new SubscriptionOptions()
+            .setStartPosition(new SubscriptionOptions.StartAtOffset(5L));
+        final List<Long> offsets = new ArrayList<Long>();
 
-    //     client.subscribe(populatedStreamName, new MessageHandler(){
-    //             public void onMessage(io.liftbridge.Message msg){
-    //                 if(msg.getValue().length > 0) {
-    //                     offsets.add(msg.getOffset());
-    //                 }
-    //             }
-    //             public void onError(Throwable t){}
-    //         }, opts);
+        client.subscribe(populatedStreamName, new MessageHandler(){
+                public void onMessage(io.liftbridge.Message msg){
+                    if(msg.getValue().length > 0) {
+                        offsets.add(msg.getOffset());
+                    }
+                }
+                public void onError(Throwable t){}
+            }, opts);
 
-    //     await().atMost(5, SECONDS).until(() -> offsets.size() >= 5);
-    //     Collections.sort(offsets);
-    //     Long[] vals = new Long[5];
-    //     assertArrayEquals("All offset, starting with 5, were received",
-    //                       new Long[]{5L, 6L, 7L, 8L, 9L},
-    //                       offsets.toArray(vals));
-    // }
+        await().atMost(5, SECONDS).until(() -> offsets.size() >= 5);
+        Collections.sort(offsets);
+        Long[] vals = new Long[5];
+        assertArrayEquals("All offset, starting with 5, were received",
+                          new Long[]{5L, 6L, 7L, 8L, 9L},
+                          offsets.toArray(vals));
+    }
 }
